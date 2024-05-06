292df3589b6048f416b27909428e81a91e2a8ee2
someone <onlyiqquee@gmail.com>
2024-05-06 09:14:42 +0100

    fix: break fixes

--- noSQL.go
+++ noSQL.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"reflect"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -14,8 +15,8 @@ type (
 	// Collection object
 	Collection struct {
 		logger         zerolog.Logger
-		CollectionName string
-		Storage        []interface{}
+		collectionName string
+		storage        []interface{}
 	}
 
 	Result struct{}
@@ -24,84 +25,86 @@ type (
 // Collection defines the collection(table) name to perform an operations on
 func (ns *NoSQL) Collection(col interface{}) *Collection {
 	t := reflect.TypeOf(col)
-	// validate t.Name()
-	if len(t.Name()) == 0 {
+
+	// run validation
+	if reflect.ValueOf(col).IsZero() && col == nil {
 		ns.logger.Error().Msg("Collection cannot be empty...")
+		panic("Collection cannot be empty...")
 	}
 
-	if t.Kind() != reflect.Struct || t.Kind() != reflect.String {
+	if t.Kind() != reflect.Struct && t.Kind() != reflect.String {
 		ns.logger.Error().Msg("Collection must either be a [string] or an [object]")
+		panic("Collection must either be a [string] or an [object]")
 	}
 
 	var colName string
+	if t.Kind() == reflect.Struct {
+		colName = strings.ToLower(t.Name())
+	} else {
+		colName = strings.ToLower(col.(string))
+	}
+
 	// check if the last index ends with the letter s
 	// if not, append (s) to it e.g user = users
-	if len(t.Name()) > 0 && string(t.Name()[len(t.Name())-1]) != "s" {
-		colName = fmt.Sprintf("%ss", t.Name())
-	} else {
-		colName = t.Name()
+	if len(colName) > 0 && string(colName[len(colName)-1]) != "s" {
+		colName = fmt.Sprintf("%ss", colName)
 	}
 
 	return &Collection{
 		logger:         ns.logger,
-		CollectionName: colName,
-		Storage:        ns.Storage,
+		collectionName: colName,
+		storage:        ns.storage,
 	}
 }
 
 // Insert adds a new record into the storage with collection name
 func (c *Collection) Insert(obj interface{}) (interface{}, error) {
-	var objMap map[string]interface{}
+	objMap := make(map[string]interface{})
 
-	v := reflect.TypeOf(obj)
-	if v.Kind() != reflect.Map {
-		jsonObj, err := json.Marshal(obj)
-		if err != nil {
-			c.logger.Err(err).Msg("JSON marshal error")
-			return nil, err
-		}
+	jsonObj, err := json.Marshal(obj)
+	if err != nil {
+		c.logger.Err(err).Msg("JSON marshal error")
+		return nil, err
+	}
 
-		if err := json.Unmarshal(jsonObj, &objMap); err != nil {
-			c.logger.Err(err).Msg("JSON unmarshal error")
-			return nil, err
-		}
+	if err := json.Unmarshal(jsonObj, &objMap); err != nil {
+		c.logger.Err(err).Msg("JSON unmarshal error")
+		return nil, err
 	}
 
 	// add additional data to the object
-	objMap["collectionName"] = c.CollectionName
+	objMap["colName"] = c.collectionName
 	objMap["id"] = uuid.New()
 	objMap["createdAt"] = time.Now()
 	objMap["deletedAt"] = nil
 
-	c.Storage = append(c.Storage, objMap)
-	return obj, nil
+	c.storage = append(c.storage, objMap)
+	return objMap, nil
 }
 
 // InsertMany adds many record into the storage at once
 func (c *Collection) InsertMany(objs []interface{}) error {
 	for _, obj := range objs {
-		var objMap map[string]interface{}
+		objMap := make(map[string]interface{})
 
-		v := reflect.TypeOf(obj)
-		if v.Kind() != reflect.Map {
-			jsonObj, err := json.Marshal(obj)
-			if err != nil {
-				c.logger.Err(err).Msg("JSON marshal error")
-				return err
-			}
+		jsonObj, err := json.Marshal(obj)
+		if err != nil {
+			c.logger.Err(err).Msg("JSON marshal error")
+			return err
+		}
 
-			if err := json.Unmarshal(jsonObj, &objMap); err != nil {
-				c.logger.Err(err).Msg("JSON unmarshal error")
-				return err
-			}
+		if err := json.Unmarshal(jsonObj, &objMap); err != nil {
+			c.logger.Err(err).Msg("JSON unmarshal error")
+			return err
 		}
 
 		// add additional data to the object
+		objMap["colName"] = c.collectionName
 		objMap["id"] = uuid.New()
 		objMap["createdAt"] = time.Now()
 		objMap["deletedAt"] = nil
 
-		c.Storage = append(c.Storage, objMap)
+		c.storage = append(c.storage, objMap)
 	}
 
 	return nil
--- noSQL_test.go
+++ noSQL_test.go
@@ -1,1 +1,48 @@
 package fscache
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var noSqlTestCases = []interface{}{
+	&struct {
+		Name string
+		Age  int
+	}{
+		Name: "Jane Doe",
+		Age:  25,
+	},
+	map[string]interface{}{
+		"name": "jane",
+		"age":  25,
+	},
+}
+
+func Test_Collection(t *testing.T) {
+	ns := NoSQL{
+		storage: noSqlTestCases,
+	}
+
+	ch := Cache{
+		NoSQL: ns,
+	}
+
+	var counter int
+	name := fmt.Sprintf("testCase %v", counter+1)
+	for _, v := range noSqlTestCases {
+		t.Run(name, func(t *testing.T) {
+			res, err := ch.NoSql().Collection("user").Insert(v)
+			if err != nil {
+				assert.Error(t, err)
+			}
+
+			fmt.Println(res)
+			assert.Equal(t, v, res)
+		})
+
+		counter++
+	}
+}
